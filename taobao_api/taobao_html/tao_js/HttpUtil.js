7014197b0ca5a49c6bea49a54f11a6bb5021a4a0
zhengbin <2223475482@qq.com>
2020-12-14 20:22:29 +0800

    正式可用已调试登录

--- taobao_api/taobao_html/tao_js/HttpUtil.js
+++ taobao_api/taobao_html/tao_js/HttpUtil.js
@@ -7,17 +7,24 @@ function appendHost(url) {
  * 对返回结果进行统一处理
  */
 function handlerResp(json, callback) {
+    // console.log("handlerResp");
+    // console.log(json);
     //返回json结果装换成对象
     let resp = JSON.parse(json);
     //判断code状态
     if (resp.code == 100) {
         //成功，调用回调方法
         callback(resp.data);
+    } else if (resp.code == 200) {
+        alert(resp.msg);
+        //用户未登录
+        location.href = "http://localhost:8080//taobao_html/登录.html";
     } else {
-        //失败，统一提示错误信息
+        //业务处理失败，统一提示错误信息
         alert(resp.msg);
     }
 }
+
 
 //get 无参
 function get(url, callback) {
